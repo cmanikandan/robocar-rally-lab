fc78175df9e8b072a71271c56d69dfbfbe4a1030
joekickass <tomas.nilsson@jayway.com>
2018-02-07 16:20:11 +0100

    Add cpu & mem reporting

--- iot/index.js
+++ iot/index.js
@@ -2,7 +2,27 @@
 
 // https://github.com/aws/aws-iot-device-sdk-js
 const thingShadow = require('aws-iot-device-sdk').thingShadow;
+const os = require('os-utils');
+
 const { Host, Port, Region, ClientId, ThingName, ThingTypeName, CaCert, ClientCert, PrivateKey } = require('/home/pi/certs/config.json');
+
+const HelloTopic = `${ThingTypeName}/hello`;
+const ReportTopic = `${ThingTypeName}/${ThingName}`
+
+let interval = 0;
+
+function top() {
+  console.log(`Reporting stats to ${ReportTopic}`);
+  const memPercentage = os.freememPercentage() * 100;
+  os.cpuUsage((cpuPercentage) => {
+    const data = {
+      cpu: cpuPercentage * 100,
+      mem: memPercentage * 100
+    };
+    shadow.publish(ReportTopic, JSON.stringify(data));
+    console.log(`${ThingName} published ${data} to '${HelloTopic}'`);
+  });
+}
 
 function run() {
   const shadow = thingShadow({
@@ -16,26 +36,31 @@ function run() {
     debug: true
   });
 
-  shadow.on('connect', function() {
+  shadow.on('connect', () => {
     console.log(`${ThingName} connected to https://${Host}:${Port}`);
-
-    shadow.register(ThingName);
-    console.log(`${ThingName} registered in Thing Registry`);
     
-    const helloTopic = `${ThingTypeName}/hello`;
-    shadow.publish(helloTopic, JSON.stringify({ Name: ThingName }));
-    console.log(`${ThingName} published its name to '${helloTopic}'`);
+    shadow.publish(HelloTopic, JSON.stringify({ Name: ThingName }));
+    console.log(`${ThingName} published its name to '${HelloTopic}'`);
+
+    console.log('Starting metric loop');
+    interval = setInterval(top, 1000);
   });
 
-  shadow.on('error', function(error) {
+  shadow.on('close', () => {
+    console.log('Stopping metric loop');
+    clearInterval(interval);
+    interval = 0;
+  });
+
+  shadow.on('error', (error) => {
     console.log('error', error);
   });
 
-  shadow.on('delta', function(thingName, stateObject) {
+  shadow.on('delta', (thingName, stateObject) => {
     console.log(`received delta on ${thingName}:${JSON.stringify(stateObject)}`);
   });
  
-  shadow.on('timeout', function(thingName, clientToken) {
+  shadow.on('timeout', (thingName, clientToken) => {
     console.warn('timeout: ' + thingName + ', clientToken=' + clientToken);
   });
 }
